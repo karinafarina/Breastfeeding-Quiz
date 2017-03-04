b5111a4691a5bc8e0f164e522b3a20fcd93830f6
Karina Gaulin <karinagaulin@gmail.com>
2017-03-04 16:17:37 -0500

    Add data

---
+++ app.js
@@ -0,0 +1,94 @@
+var myQuestions = [
+{
+	name: "First question",
+	questionText: "During the first six weeks after birth, about how many times per 24 hours should baby nurse?",
+	answers: [
+	{
+		text: "A. About 8-12 times per 24 hours/on demand.",
+	},
+	{
+		text: "B. 2-3 times per 24 hour period.",
+	},
+	{
+		text: "C. 20-30 times per 24 hour period",
+	},
+	{
+		text: "D. 6-8 times per 24 hour period."
+	}
+	]
+},
+
+{
+	name: "Second question",
+	questionText: "What should I do if baby seems hungry right after nursing?",
+	answers: [
+	{
+		text: "A. 'Top them up' with a bottle of formula.",
+	},
+	{
+		text: "B. Check that all other needs are met and then nurse the baby again.",
+	},
+	{
+		text: "C. Give them a bottle of pumped milk.",
+	},
+	{
+		text: "D. Let them cry until they fall asleep."
+	}
+	]
+},
+{
+	name: "Third question",
+	questionText: "How often should I pump after leaving the hospital?",
+	answers: [
+	{
+		text: "A. Three times per day after nursing",
+	},
+	{
+		text: "B. Three times per day before nursing",
+	},
+	{
+		text: "C. After every nursing session",
+	},
+	{
+		text: "D. Avoid the pump until two weeks before returning to work and just nurse on demand.",
+	}
+	]
+},
+{
+	name: "Forth question",
+	questionText: "What equipment or supplements are necesary for succesful breastfeeding.",
+	answers: [
+	{
+		text: "A. A nursing chair and stool and a nursing pillow.",
+	},
+	{
+		text: "B. Probiotics, Omega 3's, oatmeal, fenugreek, brewer's yeast and Mother's Milk Tea.",
+	},
+	{
+		text: "C. Your breasts.",
+	},
+	{
+		text: "D. A breast-pump, tubing and bottles.",
+	}
+	]
+},
+{
+	name: "Fifth question",
+	questionText: "If I have an alchoholic beverage, when do I need to 'pump and dump'.",
+	answers: [
+	{
+		text: "A. Right away.",
+	},
+	{
+		text: "B. Never, just wait one hour per drink for your liver to metabalize it out of your blood and milk.",
+	},
+	{
+		text: "C. An hour after consumption.",
+	},
+	{
+		text: "D. Right before nursing.",
+	}
+	]
+}
+];
+console.log("the questions");
